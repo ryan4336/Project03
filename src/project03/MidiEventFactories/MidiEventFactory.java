a01e4845acdd032850d49f0b03cfac3bb9cd4938
ryan4336 <rahhhp@umsystem.edu>
2025-04-02 16:49:24 -0500

    created midi event factory interface

---
+++ src/project03/MidiEventFactories/MidiEventFactory.java
@@ -0,0 +1,10 @@
+package project03.MidiEventFactories;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+
+public interface MidiEventFactory {
+	MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException;
+	MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException;
+	
+}
