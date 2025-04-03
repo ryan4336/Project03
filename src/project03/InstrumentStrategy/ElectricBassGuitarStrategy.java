c60f0d117be7d9dcff466ecf7ba45d328b2df4a7
ryan4336 <rahhhp@umsystem.edu>
2025-04-02 20:07:11 -0500

    Added implementation of methods in instrument strategy classes

--- src/project03/InstrumentStrategy/ElectricBassGuitarStrategy.java
+++ src/project03/InstrumentStrategy/ElectricBassGuitarStrategy.java
@@ -2,13 +2,20 @@ package project03.InstrumentStrategy;
 
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Track;
 
 public class ElectricBassGuitarStrategy implements InstrumentStrategy{
 
 	@Override
 	public void applyInstrument(Track track, int channel) throws InvalidMidiDataException {
-		
+		ShortMessage message = new ShortMessage();
+        try {
+            ((ShortMessage) message).setMessage(ShortMessage.PROGRAM_CHANGE, channel, 33, 0);
+            track.add(new MidiEvent(message, 0));
+        } catch (InvalidMidiDataException e) {
+            e.printStackTrace();
+        }
 	
 	}
 	
--- src/project03/InstrumentStrategy/TrumpetStrategy.java
+++ src/project03/InstrumentStrategy/TrumpetStrategy.java
@@ -1,13 +1,21 @@
 package project03.InstrumentStrategy;
 
 import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Track;
 
 public class TrumpetStrategy implements InstrumentStrategy{
 
 	@Override
 	public void applyInstrument(Track track, int channel) throws InvalidMidiDataException {
-		// TODO Auto-generated method stub
+		ShortMessage message = new ShortMessage();
+        try {
+            ((ShortMessage) message).setMessage(ShortMessage.PROGRAM_CHANGE, channel, 56, 0);
+            track.add(new MidiEvent(message, 0));
+        } catch (InvalidMidiDataException e) {
+            e.printStackTrace();
+        }
 		
 	}
 
