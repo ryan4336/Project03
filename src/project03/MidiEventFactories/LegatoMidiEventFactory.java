3444b0bc55a919a6ccbdd75b9eeee83f26d2da4f
ryan4336 <rahhhp@umsystem.edu>
2025-04-02 20:04:31 -0500

    added implementation details for methods in event factory classes

--- src/project03/MidiEventFactories/LegatoMidiEventFactory.java
+++ src/project03/MidiEventFactories/LegatoMidiEventFactory.java
@@ -2,6 +2,7 @@ package project03.MidiEventFactories;
 
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
 
 public class LegatoMidiEventFactory implements MidiEventFactory{
 	protected LegatoMidiEventFactory() {
@@ -10,14 +11,18 @@ public class LegatoMidiEventFactory implements MidiEventFactory{
 
 	@Override
 	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
-		
-		return null;
+		ShortMessage message = new ShortMessage();
+        message.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+        return new MidiEvent(message, tick);
 	}
 
 	@Override
 	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
-		
-		return null;
+		ShortMessage message = new ShortMessage();
+        message.setMessage(ShortMessage.NOTE_OFF, channel, note, 0);
+        return new MidiEvent(message, tick + 80);
 	}
+	
+	
 
 }
--- src/project03/MidiEventFactories/StaccatoMidiEventFactory.java
+++ src/project03/MidiEventFactories/StaccatoMidiEventFactory.java
@@ -2,6 +2,7 @@ package project03.MidiEventFactories;
 
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
 
 public class StaccatoMidiEventFactory implements MidiEventFactory{
 	protected StaccatoMidiEventFactory() {
@@ -10,14 +11,16 @@ public class StaccatoMidiEventFactory implements MidiEventFactory{
 
 	@Override
 	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
-		
-		return null;
+		ShortMessage message = new ShortMessage();
+        message.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+        return new MidiEvent(message, tick);
 	}
 
 	@Override
 	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
-		
-		return null;
+		ShortMessage message = new ShortMessage();
+        message.setMessage(ShortMessage.NOTE_OFF, channel, note, 0);
+        return new MidiEvent(message, tick - 120);
 	}
 
 }
